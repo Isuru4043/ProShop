e2bfe6daddf286f5fe235b2006bc8e10b6a56360
Isuru4043 <isuru.akalanka4043@gmail.com>
2024-12-21 16:06:27 +0530

    create header and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default App;
